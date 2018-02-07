e4036008dbeb5eaa0fb81ce39b41e9fac54373f7
Kevin Sullivan <kevin@sideapps.com>
2018-02-07 11:00:29 -0500

    SideAppsify

--- iOS-11-by-Examples/IdentityLookup/IdentityLookupViewController.swift
+++ iOS-11-by-Examples/IdentityLookup/IdentityLookupViewController.swift
@@ -17,6 +17,6 @@ class IdentityLookupViewController: UIViewController {
     }
     
     @IBAction func filterButtonAction(_ sender: Any) {
-        UserDefaults(suiteName: "group.com.artemnovichkov.iOS-11-by-Examples")?.blockedNumber = textField.text
+        UserDefaults(suiteName: "group.com.sideapps.iOS-11-by-Examples")?.blockedNumber = textField.text
     }
 }
